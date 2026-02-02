58896256f85e6581120cb21942027c80ba2febca
Teagan Dorsch <teagandorsch@gmail.com>
2026-02-02 14:37:58 +0000

    get current season from region and date

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,10 +1,48 @@
-fetch("data/plants.yaml")
-  .then(response => response.text())
-  .then(text=> {
-    const plants = jsyaml.load(text);
-    console.log(plants);
+function getSeasonFromOffset(daysFromLastFrost, seasons) {
+  for (const key in seasons) {
+    const season = seasons[key];
 
-    plants.forEach(plant => {
-      console.log(plant.name);
-    });
-  })
+    if (
+      daysFromLastFrost >= season.start_offset_days &&
+      daysFromLastFrost <= season.end_offset_days
+    ) {
+      return key;
+    }
+  }
+  return null;
+}
+
+Promise.all([
+  fetch("data/plants.yaml").then(r => r.text()),
+  fetch("data/regions.yaml").then(r => r.text()),
+  fetch("data/seasons.yaml").then(r => r.text())
+])
+.then(([plantsText, regionsText, seasonsText]) => {
+  const plants = jsyaml.load(plantsText);
+  const regions = jsyaml.load(regionsText);
+  const seasons = jsyaml.load(seasonsText);
+
+  console.log("Plants:", plants);
+  console.log("Regions:", regions);
+  console.log("Seasons:", seasons);
+
+  const region = regions.find(r => r.id === "north_west_england");
+  console.log("Region in use:", region);
+
+  const today = new Date();
+  const lastFrost = new Date(region.avg_last_frost);
+
+  const msPerDay = 1000 * 60 * 60 * 24;
+  const daysFromLastFrost = Math.floor(
+    (today - lastFrost) / msPerDay
+  );
+
+  console.log("Days from last frost:", daysFromLastFrost)
+
+  const currentSeason = getSeasonFromOffset(
+    daysFromLastFrost,
+    seasons
+  );
+
+  console.log("Current season:", currentSeason);
+})
